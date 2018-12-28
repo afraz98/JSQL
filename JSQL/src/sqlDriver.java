3dcd7f65b010540fb6b571fba13432086982b09a
ajax <ajax@10.0.0.61>
2018-12-28 03:16:58 -0500

    Added method to retrieve data from table

--- JSQL/src/sqlDriver.java
+++ JSQL/src/sqlDriver.java
@@ -9,6 +9,7 @@ public class sqlDriver {
 	private String user;
 	private String password; 
 	private Connection c;
+	private ResultSet r; 
 	
 	public sqlDriver(String URL, String user, String password) {
 		this.URL = URL;
@@ -63,6 +64,24 @@ public void sendQuery(PreparedStatement ps) throws Exception{
 	
 }
 
+public void getData(String query) throws SQLException {
+	Statement s; 
+	try {
+		s = c.createStatement();
+		ResultSet r = s.executeQuery(query); 
+		while(r.next()) {
+			int ID = r.getInt("id");
+			String name = r.getString("name");
+			String birthday = r.getString("birthday");
+			String email = r.getString("email");
+			System.out.println(ID + "\t" + name + "\t" + birthday + "\t" + email);
+		}
+	} catch(Exception e) {
+		e.printStackTrace();
+	}
+	c.close();
+}
+
 	public static void main(String[] args) throws Exception {
 	    String user, password, ip, port, database, URL; 
 		Scanner scan = new Scanner(System.in); 
@@ -82,7 +101,8 @@ public void sendQuery(PreparedStatement ps) throws Exception{
 		URL = "jdbc:mysql://" + ip + ":" + port + "/" + database;
 		sqlDriver x = new sqlDriver(URL, user, password); 
 		x.connectSQL();
-		x.sendQuery(x.insertData("friendsList(id, name, birthday)"));
+		//x.sendQuery(x.insertData("friendsList(id, name, birthday)"));
+		x.getData("SELECT * FROM friendsList;");
 		scan.close(); 
 	}
 }
